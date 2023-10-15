94929d60e30a7d85f8acc8a1fc6ff8f709b6335a
geako <geakos98@gmail.com>
2023-10-15 12:21:04 +0200

    Added Predictions - Ákos

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import random
 import pandas as pd  # Import Pandas for data processing
 
 # Function to process the uploaded file (you can replace this with your processing logic)
@@ -198,15 +199,15 @@ def plot_confusion_matrix(df, real, predicted):
 
 
 def plot_roc_curve(predictions_df):
-    # Extract the 'Real', 'Pred', and 'Prob' columns
+    # Extract the 'Real' and 'Prob' columns
     ground_truth_values = predictions_df['edge'].tolist()
-    predictions = predictions_df['pred_edge'].tolist()
+    probabilities = predictions_df['prob'].tolist()  # Use 'Prob' column
 
     # Calculate the ROC curve
-    fpr, tpr, _ = roc_curve(ground_truth_values, predictions)
+    fpr, tpr, _ = roc_curve(ground_truth_values, probabilities)
 
     # Calculate the AUC (Area Under the Curve)
-    roc_auc = roc_auc_score(ground_truth_values, predictions)
+    roc_auc = roc_auc_score(ground_truth_values, probabilities)
 
     # Plot the ROC curve
     plt.figure(figsize=(8, 6))
@@ -229,15 +230,49 @@ def plot_histogram(df):
     plt.grid(axis='y', linestyle='--', alpha=0.7)  # Add grid lines
     plt.show()
 @st.cache_data
-def make_predictions(df, node, recommendation_number=5):
+def make_predictions(df, node, mapper, recommendation_number=5):
+    text = "Calculating..."
+
     offers = df[(df['edge'] == 0) & (df['pred_edge'] == 1)].reset_index()
 
     filtered_offers = offers[(offers['first_node'] == node) | (offers['second_node'] == node)]
 
     filtered_offers['neighbor'] = filtered_offers.apply(lambda x: x['first_node'] if x['first_node'] != node else x['second_node'], axis = 1).astype('int')
 
-    st.write(list(filtered_offers.sort_values('prob', ascending = False)['neighbor'][:recommendation_number]))
+    text = 'Recommended Friends: '
+    for elem in filtered_offers.sort_values('prob', ascending = False)['neighbor'][:recommendation_number].values:
+        text += str(mapper[elem])
 
+        text += ', '
+
+    text = text[:-2]
+
+    st.write(text)
+
+@st.cache_data
+def create_plots(df):
+    plot_histogram(df)
+    st.pyplot()
+
+    plot_confusion_matrix(df, 'edge', 'pred_edge')
+    st.pyplot()
+
+    plot_roc_curve(df)
+    st.pyplot()
+
+
+def get_possible_nodes(options):
+    sample_num = 3  # Number of elements to sample
+    possible_nodes = random.sample(options, sample_num)
+
+    text = 'Node ID examples: '
+    for elem in possible_nodes:
+        text += str(elem)
+        text += ', '
+
+    text = text[:-2]
+
+    st.write(text)
 
 
 @st.cache_data
@@ -256,12 +291,12 @@ def main_process(file):
     predictions = create_result_df(splitted_graph, model, embedding_all, operator_l2)
 
     st.write('Finished Preprocessing')
-    return predictions.reset_index()
+    return predictions.reset_index(), mapper, reverse_mapper
 
-st.title("EDGES File Upload and Processing App")
+st.title("Graph Recommender System")
 
 # Create a file uploader widget for EDGES files (you can specify other file types)
-uploaded_file = st.file_uploader("Upload an EDGES file", type=[".edges"])
+uploaded_file = st.file_uploader("Upload a .edges file", type=[".edges"])
 
 
 if uploaded_file is not None:
@@ -272,30 +307,35 @@ if uploaded_file is not None:
     # start_processing_button = st.button("Start Processing", key="embedding_button")
 
 
+
     # if start_processing_button:
-    predictions = main_process(uploaded_file)
-
-    plot_histogram(predictions)
-    st.pyplot()
-
-    plot_confusion_matrix(predictions, 'edge', 'pred_edge')
-    st.pyplot()
-
-    plot_roc_curve(predictions)
-    st.pyplot()
-
-
+    predictions, mapper, reverse_mapper = main_process(uploaded_file)
 
     if predictions is not None:
-        # Ask for a node_id
-        node_id = st.number_input("Enter a Node ID for Recommendations", min_value=0, max_value=max([max(predictions['first_node']), max(predictions['first_node'])]))
+        # max_value = max([max(predictions['first_node']), max(predictions['first_node'])]))
+        max_value = max(list(mapper.values()))
 
-        st.write(f'Selected Node ID {node_id}')
+        # Ask for a node_id
+        node_id = st.number_input("Enter a Node ID for making recommendations:", min_value=0, max_value=max_value)
+
+
+    get_possible_nodes(list(mapper.values()))
+
 
     get_recommendations_button = st.button("Get Recommendations", key="recommendation_button")
 
-    if get_recommendations_button and predictions is not None:
-        make_predictions(predictions, node_id)
+    try:
+        mapped_node_id = reverse_mapper[node_id]
+        if get_recommendations_button and predictions is not None:
+            make_predictions(predictions, mapped_node_id, mapper)
+
+    except KeyError:
+        st.write("This Node ID doesn't exist")
+
+
+    create_plots(predictions)
+
 
         # Display the other plots and charts here
 
+# TODO: Mapping Elvégzése
